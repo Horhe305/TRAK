9df700943381bc71c0d8b933994d7314e1de1ef7
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-06-20 12:55:29 +0000

    git

--- Main.java
+++ Main.java
@@ -3,40 +3,9 @@ class Main
 {
   public static void main(String[] args)
   {
-    Scanner input = new Scanner(System.in);
-    Scanner Obj = new Scanner(System.in); 
-    System.out.println("Witaj! Co chcesz zrobić?\n");
-    System.out.println("1: Wyswietl liczby parzyste od 0 do 100");
-    System.out.println("2: Sprawdz czy liczba jest pierwsza");
-
-
-  int liczba = input.nextInt();
-  switch (liczba)
-  {
-    case 1:
      for (int i=0; i<=100; i++)
        {
          if (i%2==0) System.out.println(i);
-       }
-    break;
-      
-    case 2:
-     String s;
-    System.out.println("Wpisz tekst: ");
-    s = Obj.nextLine();
-    char[] tekst = s.toCharArray();
-    char tab[] = new char[100];
-
-    for (int i = 0; i < tekst.length; i++)
-      {
-        if (tekst[i] == 32) tekst[i] = 95;
-        tab[i] = tekst[i];
-        System.out.print(tab[i]);
-      }
- break; 
-      
-    default:
-      System.out.println("Spróbuj raz jeszcze"); 
-  }
+       }   
   }
 }
